44e8284fa1cc4c148417aa0290fcea3e2a52f511
Sivan Mehta <sivanmehta@microsoft.com>
2023-01-23 16:59:40 -0800

    strikethrough

--- src/Board.js
+++ src/Board.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { useGame, CELL_SIZE } from './constants';
-
-function checkWinner() {
-
-}
+import { useGame, CELL_SIZE, checkWinner } from './constants';
+import GameOver from './GameOver';
 
 function Cell({ x, y, content, onClick}) {
   return (
@@ -16,7 +13,6 @@ function Cell({ x, y, content, onClick}) {
       </text>
       <rect
         className='cell'
-        key={`cell ${i}-${j}`}
         x={x * CELL_SIZE + 1}
         y={y * CELL_SIZE + 1}
         width={CELL_SIZE}
@@ -31,45 +27,36 @@ export default function Board() {
   const [ state, setState ] = useGame();
   const cells = [];
 
-  if(state.over) {
-    return (
-      <GameOver />
-    )
+  let overlay = "";
+  if (state.over) {
+    overlay = <GameOver />;
   }
+
+  console.log(state.over);
 
   function onClick(x, y) {
     if(state.board[y][x] !== '') return;
+    if(state.over) return;
     
     const newBoard = state.board.map(row => row.slice());
     newBoard[y][x] = state.player;
     setState({
       board: newBoard,
       player: state.player === 'x' ? 'o' : 'x',
+      over: checkWinner(newBoard, state.turn),
+      turn: state.turn + 1
     });
-
-    checkWinner(newBoard);
   }
 
   state.board.forEach((row, y) => {
-    row.forEach((cell, x) => {
+    row.forEach((content, x) => {
       cells.push(
-        <>
-          <text
-            x={ x * CELL_SIZE + 1 + CELL_SIZE / 2 }
-            y={ y * CELL_SIZE + 1 + 3 * CELL_SIZE / 4 }
-          >
-            {cell}
-          </text>
-          <rect
-            className='cell'
-            key={`cell ${x}-${y}`}
-            x={x * CELL_SIZE + 1}
-            y={y * CELL_SIZE + 1}
-            width={CELL_SIZE}
-            height={CELL_SIZE}
-            onClick={() => onClick(x, y)}
-          />
-        </>
+        <Cell
+          key={`cell ${x}-${y}`}
+          x={x}
+          y={y}
+          content={content}
+          onClick={ () => onClick(x, y)}/>
       );
     });
   })
@@ -78,6 +65,7 @@ export default function Board() {
     <svg viewBox={`0 0 360 360`}>
       <rect x="0" y = "0" width="360" height="360"/>
       { cells }
+      { overlay }
     </svg>
   )
 }
--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,45 @@ import React, { createContext, useState, useContext } from 'react';
 
 export const CELL_SIZE = 120;
 
+// there's got to be a better way to do this
+export function checkWinner(board, turn) {
+  const potentialWinner = turn % 2 === 0 ? 'x' : 'o';
+  console.log('checking winner', potentialWinner, board);
+  // horizontal win
+  for(let i = 0; i < 3; i++) {
+    if(board[i][0] === potentialWinner &&
+      board[i][1] === potentialWinner &&
+      board[i][2] === potentialWinner) {
+      return [potentialWinner, 'horizontal', i];
+    }
+  }
+
+  // vertical win
+  for(let i = 0; i < 3; i++) {
+    if(board[0][i] === potentialWinner &&
+      board[1][i] === potentialWinner &&
+      board[2][i] === potentialWinner) {
+      return [potentialWinner, 'vertical', i];
+    }
+  }
+
+  // diagonal win
+  if(board[0][0] === potentialWinner &&
+    board[1][1] === potentialWinner &&
+    board[2][2] === potentialWinner) {
+    return [potentialWinner, 'diagonal', 0];
+  }
+
+  // other diagonal win
+  if(board[0][2] === potentialWinner &&
+    board[1][1] === potentialWinner &&
+    board[2][0] === potentialWinner) {
+    return [potentialWinner, 'diagonal', 1];
+  }
+
+  return false;
+}
+
 let board = [
   ['', '', ''],
   ['', '', ''],
@@ -12,6 +51,7 @@ const baseGame = {
   player: 'x', // which player is playing
   over: false, // if the game is over (indicates when the score needs to be calculated)
   board, // pieces placed on the board
+  turn: 0
 };
 
 const Game = createContext(baseGame);
