48926cabd1d3644179f907e6f49b8da88f9f0b47
Sivan Mehta <sivanmehta@microsoft.com>
2023-02-22 14:20:19 -0800

    support UI routing

--- packages/server/main.js
+++ packages/server/main.js
@@ -4,12 +4,16 @@ import { createServer } from 'http';
 import morgan from 'morgan';
 import path from 'path';
 
-const staticDirectory = path.join('..', 'client', 'dist');
-
 const app = express();
-app.use(express.static(staticDirectory));
-app.use('/game/:gameId', express.static(staticDirectory))
 app.use(morgan('dev'));
+
+const staticDirectory = path.join('..', 'client', 'dist');
+const UIRouter = express.static(staticDirectory);
+const UIRoutes = [
+  '/',
+  '/game/:gameId'
+];
+UIRoutes.forEach(route => app.use(route, UIRouter));
 
 const server = createServer(app);
 const wss = new WebSocketServer({ server });
@@ -17,7 +21,6 @@ const wss = new WebSocketServer({ server });
 wss.on('connection', function (ws) {
   console.log('new connection!')
   ws.send(JSON.stringify(process.memoryUsage()));
-  console.log('started client interval');
 
   ws.on('error', console.error);
 
